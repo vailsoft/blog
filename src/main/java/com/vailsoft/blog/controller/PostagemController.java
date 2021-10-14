7fdcaa0296e6e6de45fbf76d53e74accac6c7463
vails <vailsoft@hotmail.com>
2021-10-14 15:48:14 -0300

    Aula 14/10/2021

---
+++ src/main/java/com/vailsoft/blog/controller/PostagemController.java
@@ -0,0 +1,15 @@
+package com.vailsoft.blog.controller;
+
+import com.vailsoft.blog.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagem")
+@CrossOrigin("*")
+public class PostagemController {
+    @Autowired
+    private PostagemRepository repository;
+}
---
+++ src/main/java/com/vailsoft/blog/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package com.vailsoft.blog.repository;
+
+import com.vailsoft.blog.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+    // Consulta por titulo
+    public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+}
