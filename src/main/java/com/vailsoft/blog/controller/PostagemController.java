2a9cba811b5dfda160afa51d411353ccfe24a8eb
vails <vailsoft@hotmail.com>
2021-10-15 10:39:28 -0300

    Selecionando pelo id

--- src/main/java/com/vailsoft/blog/controller/PostagemController.java
+++ src/main/java/com/vailsoft/blog/controller/PostagemController.java
@@ -4,10 +4,7 @@ import com.vailsoft.blog.model.Postagem;
 import com.vailsoft.blog.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,11 @@ public class PostagemController {
     @GetMapping
     public ResponseEntity<List<Postagem>> GetAll() {
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> GetById(@PathVariable Long id){
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
     }
 }
 
